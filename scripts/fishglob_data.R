d80b86db2dc2b116d932819a84d82243f89e7ec5
enazario11 <enazario@ucsc.edu>
2026-05-23 18:17:09 -0700

    fishglob metdat

--- scripts/fishglob_data.R
+++ scripts/fishglob_data.R
@@ -2,77 +2,72 @@
 #libraries
 library(tidyverse)
 library(here)
+library(lubridate)
 
 #load dataset
-dat_glob <- readRDS(here("data/fishglob/FishGlob_public_metadata_clean.RDS"))
-dat_glob_nam <- dat_glob %>% filter(continent == 'n_america')
-
-#striped sea bass 
-dat_glob_bass <- dat_glob_nam %>% filter(accepted_name == 'Morone saxatilis')
-table(dat_glob_bass$survey) #only included for Northeast US NOAA surveys, 1 Gulf St. Lawrence South
-
-# atlantic cod
-dat_glob_cod <- dat_glob_nam %>% filter(accepted_name == 'Gadus morhua')
-table(dat_glob_cod$survey) #included for Northeast US NOAA surveys, Gulf St. Lawrence, and Canada Maritimes
-
+dat_glob <- readRDS(here("data/fishglob/fishglob_usa.rds")) %>% filter(survey == "NEUS" | survey == "SEUS" | survey == "WCANN" | survey == "WCTRI")
+dat_spp <- read.csv(here("data/fishglob/spp_metdat.csv"))
 
 # MI/AGI species list -----------------------------------------------------
+for(i in 1:nrow(dat_spp)){
+  #filter df for given species
+  curr_sp <- paste(dat_spp$Genus[i], dat_spp$Species[i])
+  temp_glob <- dat_glob %>% filter(accepted_name == curr_sp)
 
-met_sp <- data.frame(sp_names = gsub("\t", " ", c("Anguilla	japonica",
-               "Carassius	carassius",
-               "Centropristis	striata",
-               "Cyclopterus	lumpus",
-               "Cyprinus	carpio",
-               "Dentex	dentex",
-               "Etheostoma	flabellare",
-               "Gadus	morhua",
-               "Gadus	macrocephalus",
-               "Lates	calcarifer",
-               "Morone	saxatilis",
-               "Oncorhynchus	mykiss",
-               "Oreochromis	niloticus",
-               "Ostorhinchus	doederleini",
-               "Parabramis	pekinensis",
-               "Paralichthys	dentatus",
-               "Pomacentrus	moluccensis",
-               "Salmo	salar",
-               "Sciaenops	ocellatus",
-               "Scyliorhinus	canicula",
-               "Seriola	lalandi",
-               "Squalas	acanthias",
-               "Stenobrachius	leucopsarus",
-               "Tautogolabrus	adspersus",
-               "Chrysoblephus	laticeps",
-               "Rachycentron	canadum")))
+  #extract metadata for given species
+  dat_spp$rows[i] <- nrow(temp_glob)
+  dat_spp$min_year[i] <- min(temp_glob$year, na.rm = TRUE)
+  dat_spp$max_year[i] <- max(temp_glob$year, na.rm = TRUE)
+  dat_spp$n_year[i] <- sum(length(unique(temp_glob$year)))
+  dat_spp$survey_1[i] <- unique(temp_glob$survey_unit)[1]
+  dat_spp$survey_2[i] <- unique(temp_glob$survey_unit)[2]
+  dat_spp$survey_3[i] <- unique(temp_glob$survey_unit)[3]
+  dat_spp$survey_4[i] <- unique(temp_glob$survey_unit)[4]
+  dat_spp$survey_5[i] <- unique(temp_glob$survey_unit)[5]
 
-met_sp$fish_glob = "NA"
-for(i in 1:nrow(met_sp)){
-  sp_temp = met_sp$sp_names[i]
-  
-  fish_glob = sum(str_detect(dat_glob$accepted_name, sp_temp)) > 0
-  
-  met_sp$fish_glob[i] <- fish_glob
 }
 
-#met_glob_filter <- met_sp %>% filter(fish_glob == "FALSE")
+write.csv(dat_spp, here("data/temp/glob_metdat.csv"))
 
+# look at depth layers for pelagic/schooling fish
+temp_glob <- dat_glob %>% filter(accepted_name == "Clupea harengus")
+plot(temp_glob$depth, temp_glob$num) #50m
 
-# filter fishglob ---------------------------------------------------------
-met_glob_sp <- met_sp %>% filter(fish_glob == "TRUE")
+temp_glob <- dat_glob %>% filter(accepted_name == "Scomber scombrus")
+plot(temp_glob$depth, temp_glob$num) #50m
 
-dat_glob_met <- filter(dat_glob, str_detect(accepted_name, paste(met_glob_sp$sp_names, collapse = "|")))
+temp_glob <- dat_glob %>% filter(accepted_name == "Pomatomus saltatrix")
+plot(temp_glob$depth, temp_glob$num) #25m
 
-#check if match
-SameElements <- function(a, b) return(identical(sort(a), sort(b)))
+temp_glob <- dat_glob %>% filter(accepted_name == "Peprilus triacanthus")
+plot(temp_glob$depth, temp_glob$num) #100m
 
-SameElements(unique(dat_glob_met$accepted_name), unique(met_glob_sp$sp_names)) #TRUE
+temp_glob <- dat_glob %>% filter(accepted_name == "Scomberomorus cavalla")
+plot(temp_glob$depth, temp_glob$num) #25m
 
+temp_glob <- dat_glob %>% filter(accepted_name == "Engraulis mordax")
+plot(temp_glob$depth, temp_glob$num) #25m
 
-# explore fishglob --------------------------------------------------------
+temp_glob <- dat_glob %>% filter(accepted_name == "Clupea pallasii")
+plot(temp_glob$depth, temp_glob$num) #100m
 
-survey_n <- dat_glob_met %>% group_by(accepted_name, survey) %>% summarise(n = n()) #tally by species and survey unit
+temp_glob <- dat_glob %>% filter(accepted_name == "Scomber japonicus")
+plot(temp_glob$depth, temp_glob$num) #100m
 
-dat_glob_met %>% group_by(accepted_name) %>% summarise(n = n()) %>% filter(n > 5) #tally by species, removed Dentex dentex (n = 2) and Oncorhynchus mykiss (n = 1)
+temp_glob <- dat_glob %>% filter(accepted_name == "Sardinops sagax")
+plot(temp_glob$depth, temp_glob$num) #100m
 
+temp_glob <- dat_glob %>% filter(accepted_name == "Epinephelus morio")
+plot(temp_glob$depth, temp_glob$num) #50m
 
+temp_glob <- dat_glob %>% filter(accepted_name == "Urophycis chuss")
+plot(temp_glob$depth, temp_glob$num) #100m
 
+temp_glob <- dat_glob %>% filter(accepted_name == "Stenotomus chrysops")
+plot(temp_glob$depth, temp_glob$num) #25m
+
+temp_glob <- dat_glob %>% filter(accepted_name == "Merluccius bilinearis")
+plot(temp_glob$depth, temp_glob$num) #1000m
+
+temp_glob <- dat_glob %>% filter(accepted_name == "Scomberomorus maculatus")
+plot(temp_glob$depth, temp_glob$num) #50m
